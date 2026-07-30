9ddc477b631569e3be0da698c8e1b3a72405a9b4
hoangnvthkt <hoangnvthkt@gmail.com>
2026-07-30 08:29:49 +0700

    fix(layout): restore scrolling on full-bleed routes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -226,7 +226,7 @@ const Layout: React.FC = () => {
           </div>
         </header>
 
-        <main className={isFullBleedRoute ? "flex-1 overflow-hidden relative" : "flex-1 overflow-auto p-2 sm:p-4 md:p-8 pb-20 lg:pb-8 transparent"}>
+        <main className={isFullBleedRoute ? "flex-1 min-h-0 overflow-auto relative" : "flex-1 overflow-auto p-2 sm:p-4 md:p-8 pb-20 lg:pb-8 transparent"}>
           {isLoading || isRefreshing ? (
             <div className="h-full w-full flex flex-col items-center justify-center relative overflow-hidden">
               {/* Background animated particles */}
